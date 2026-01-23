7a108f05943fd8f7f5e304798dff587181d55517
FrostyCoffee <taradapabintang@gmail.com>
2026-01-23 19:03:44 +0700

    minor routing changes lagi mff yh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./app/Login";
 import UserDashboard from "./app/UserDashboard";
 import Admin from "./app/Admin";
+import Dashboard from "./app/Dashboard";
 import { Header, ProtectedRoute } from "./components/Common";
 
 function App() {
@@ -32,12 +33,25 @@ function App() {
 
         {/* ADMIN DASHBOARD */}
         <Route
-          path="/dashboard"
+          path="/admin"
           element={
             <ProtectedRoute allowedRoles={["admin"]}>
               <>
                 <Header />
                 <Admin />
+              </>
+            </ProtectedRoute>
+          }
+        />
+
+        {/* DASHBOARD */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["admin"]}>
+              <>
+                <Header />
+                <Dashboard />
               </>
             </ProtectedRoute>
           }
