1573b7834297c2ca9acc1864b4766a3b6e18e5eb
Reza <rezahanif2005@gmail.com>
2026-02-04 01:41:36 +0700

    tes pack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,15 @@ import Login from "./app/Login";
 import UserDashboard from "./app/UserDashboard";
 import AdminLayout from "./app/AdminLayout";
 import Dashboard from "./components/Admin/Dashboard";
+import Members from "./components/Admin/Members";
+import AddBook from "./components/Admin/AddBook";
+import BorrowBook from "./components/Admin/BorrowBook";
 import { Header, ProtectedRoute } from "./components/Common";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* DEFAULT */}
-        <Route path="/" element={<UserDashboard />} />
         {/* LOGIN */}
         <Route path="/login" element={<Login />} />
 
@@ -28,7 +28,7 @@ function App() {
           }
         />
 
-        {/* ADMIN (LAYOUT + OUTLET) */}
+        {/* ADMIN */}
         <Route
           path="/admin"
           element={
@@ -37,15 +37,18 @@ function App() {
             </ProtectedRoute>
           }
         >
+          <Route index element={<Navigate to="dashboard" replace />} />
           <Route path="dashboard" element={<Dashboard />} />
-          {/* nanti tambah members, addBook, borrowBook di sini */}
+          <Route path="members" element={<Members />} />
+          <Route path="addBook" element={<AddBook />} />
+          <Route path="borrowBook" element={<BorrowBook />} />
         </Route>
 
         {/* DEFAULT */}
-        <Route path="/" element={<Navigate to="/login " />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
         {/* FALLBACK */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
