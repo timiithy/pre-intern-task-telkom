489bc7a80770939771774a98ec2be01bf4a83f8d
Reza <rezahanif2005@gmail.com>
2026-02-03 12:24:44 +0700

    anjay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,44 +3,42 @@ import Login from "./app/Login";
 import UserDashboard from "./app/UserDashboard";
 import Admin from "./app/Admin";
 import Dashboard from "./app/Dashboard";
-import { Header, ProtectedRoute } from "./components/Common";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* DEFAULT - User Dashboard */}
-        <Route path="/" element={<UserDashboard />} />
+
+        {/* DEFAULT → DASHBOARD ADMIN */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Dashboard />
+            </>
+          }
+        />
 
         {/* LOGIN */}
         <Route path="/login" element={<Login />} />
-
-        {/* USER DASHBOARD - Public Access */}
-        <Route path="/dashboard-user" element={<UserDashboard />} />
 
         {/* USER DASHBOARD */}
         <Route
           path="/dashboard-user"
           element={
-            <ProtectedRoute allowedRoles={["user"]}>
-              <>
-                <Header />
-                <UserDashboard />
-              </>
-            </ProtectedRoute>
+            <>
+              <UserDashboard />
+            </>
           }
         />
 
-        {/* ADMIN DASHBOARD */}
+        {/* ADMIN PAGE */}
         <Route
           path="/admin"
           element={
-            <ProtectedRoute allowedRoles={["admin"]}>
-              <>
-                <Header />
-                <Admin />
-              </>
-            </ProtectedRoute>
+            <>
+              <Admin />
+            </>
           }
         />
 
@@ -48,20 +46,18 @@ function App() {
         <Route
           path="/dashboard"
           element={
-            <ProtectedRoute allowedRoles={["admin"]}>
-              <>
-                <Header />
-                <Dashboard />
-              </>
-            </ProtectedRoute>
+            <>
+              <Dashboard />
+            </>
           }
         />
 
         {/* FALLBACK */}
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default App
