3f1ac59b0e5fb4006cf12304b6e6fb70ac3fd7b2
Reza <rezahanif2005@gmail.com>
2026-02-03 14:50:56 +0700

    benerin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
         </Route>
 
         {/* DEFAULT */}
-        <Route path="/" element={<Navigate to="/dashboard-user" />} />
+        <Route path="/" element={<Navigate to="/login " />} />
 
         {/* FALLBACK */}
         <Route path="*" element={<Navigate to="/" replace />} />
